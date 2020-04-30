33e15aa5c1e30438ee3f2ebdb551aee3df46bffe
zhangqi <zh1402717363@sina.com>
2020-04-30 14:52:11 +0800

    vue-admin-demo update1

---
+++ src/store/modules/user.js
@@ -0,0 +1,100 @@
+import { login, getInfo } from '@/api/user'
+import { getToken, setToken, removeToken } from '@/utils/auth'
+import { resetRouter } from '@/router'
+
+const getDefaultState = () => {
+  return {
+    token: getToken(),
+    name: '',
+    avatar: ''
+  }
+}
+
+const state = getDefaultState()
+
+const mutations = {
+  RESET_STATE: (state) => {
+    Object.assign(state, getDefaultState())
+  },
+  SET_TOKEN: (state, token) => {
+    state.token = token
+  },
+  SET_NAME: (state, name) => {
+    state.name = name
+  },
+  SET_AVATAR: (state, avatar) => {
+    state.avatar = avatar
+  }
+}
+
+const actions = {
+  // user login
+  login({ commit }, data) {
+    // const { username, password } = userInfo
+    return new Promise((resolve, reject) => {
+      login(data).then(response => {
+        const { data } = response
+        commit('SET_TOKEN', data.user.uuid)
+        setToken(data.user.uuid)
+        resolve()
+      }).catch(error => {
+        reject(error)
+      })
+    })
+  },
+
+  // get user info
+  getInfo({ commit, state }) {
+    return new Promise((resolve, reject) => {
+      getInfo().then(response => {
+        const { data } = response
+
+        if (!data) {
+          reject('校验错误，请重新登录')
+        }
+
+        const { user } = data
+
+        commit('SET_NAME', user.nickName)
+        commit('SET_AVATAR', user.purikura)
+        resolve(data)
+      }).catch(error => {
+        reject(error)
+      })
+    })
+  },
+
+  // user logout
+  logout({ commit, state }) {
+    removeToken() // must remove  token  first
+    resetRouter()
+    commit('RESET_STATE')
+    // return new Promise((resolve, reject) => {
+    //   logout(state.token).then(() => {
+    //     removeToken() // must remove  token  first
+    //     resetRouter()
+    //     commit('RESET_STATE')
+    //     resolve()
+    //   }).catch(error => {
+    //     reject(error)
+    //   })
+    // })
+  },
+
+  // remove token
+  resetToken({ commit }) {
+    return new Promise(resolve => {
+      removeToken() // must remove  token  first
+      commit('RESET_STATE')
+      resolve()
+    })
+  }
+}
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions
+}
+
